45848d73aacda9a7fda86eb33a8a21ed7e939873
Joseph Lambourne <joseph.lambourne@autodesk.com>
2021-04-26 20:10:12 +0100

    Implement closest point stuff

--- occwl/shape.py
+++ occwl/shape.py
@@ -1,6 +1,13 @@
 """
 Base class for faces, edges and vertices
 """
+# OCC
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeVertex
+from OCC.Core.BRepExtrema import BRepExtrema_DistShapeShape
+from OCC.Core.Extrema import Extrema_ExtFlag_MIN
+
+# occwl
+import occwl.geometry.geom_utils as geom_utils
 
 class ClosestPointData:
     """
@@ -8,18 +15,41 @@ class ClosestPointData:
     to some datum point
     """
 
-    def __init__(
-        closest_entity,  # The closest sub-shape to the point
-        closest_point,   # The closest point on the shape
-        distance         # The distance to the closest point
-    ):
-    self.closest_entity = closest_entity
-    self.closest_point = closest_point
-    self.distance = distance
+    def __init__(self, dist_shape_shape):
+        """
+        Args:
+            dist_shape_shape (BRepExtrema_DistShapeShape): OCC class for distance to a shape
+        """
+        assert dist_shape_shape.IsDone()
+        self.closest_entity = dist_shape_shape.SupportOnShape2(1)
+        self.closest_point = geom_utils.gp_to_numpy(dist_shape_shape.PointOnShape2(1))
+        self.distance = dist_shape_shape.Value()
 
 class Shape:
 
-    def find_closest_point_data(datum):
+    def find_closest_point_data(self, datum):
         """
         Find the information about the closest point on this shape
+        
+        Args:
+            datum (np.ndarray): 3D Point
+        
+        Returns:
+            ClosestPointData: Data about the closest point on this shape
+            None: if error
         """
+        # Folowing https://dev.opencascade.org/content/how-retrieve-nearest-face-shape-given-gppnt
+        # Create a vertex from the point
+        occ_point = geom_utils.numpy_to_gp(datum)
+        vertex_maker = BRepBuilderAPI_MakeVertex(occ_point)
+        vertex = vertex_maker.Shape()
+        dist_shape_shape = BRepExtrema_DistShapeShape(
+            vertex, 
+            self.topods_shape(),
+            Extrema_ExtFlag_MIN
+        )
+        ok = dist_shape_shape.Perform()
+        if not ok:
+            return None
+
+        return ClosestPointData(dist_shape_shape)
