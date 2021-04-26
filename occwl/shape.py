1100c343c51d1b67b91d3143aaf2abb8e63bbfea
Joseph Lambourne <joseph.lambourne@autodesk.com>
2021-04-26 13:31:19 +0100

    Rename enity to shape.  i.e. follow OCC naming or we get into a mess

--- occwl/shape.py
+++ occwl/shape.py
@@ -9,16 +9,17 @@ class ClosestPointData:
     """
 
     def __init__(
-        closest_entity,  # The closest entity to the point
-        closest_point,   # The closest point on the entity
-        distance
+        closest_entity,  # The closest sub-shape to the point
+        closest_point,   # The closest point on the shape
+        distance         # The distance to the closest point
     ):
     self.closest_entity = closest_entity
     self.closest_point = closest_point
     self.distance = distance
 
-class Entity:
+class Shape:
 
     def find_closest_point_data(datum):
         """
-        Find the closest point and closest 
+        Find the information about the closest point on this shape
+        """
