ee6c66e7117154979cafe9db8b7dfaff6661bfbf
Joseph Lambourne <joseph.lambourne@autodesk.com>
2021-04-26 20:13:26 +0100

    Run the edge data extractor

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -6,6 +6,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # OCC
+from test_edge_data_extractor import EdgeDataExtractorTester
 from test_grid_and_normals import GridTester
 from test_box import BoxTester
 from test_edge import EdgeTester
