07ab711d0cf9a30aa184070a9a4d61d2e08c70ea
Kamal Rahimi Malekshan <kamal.malekshan@autodesk.com>
2021-05-20 16:28:05 -0400

    Add Bash Script to Build Conda Package

---
+++ build_conda_package.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# This script builds a conda package and uploads 
+# package to an anaconda channel if anaconda username
+# is provided as an input argument to the script.
+# Note conda package will only be uploaded to
+# anaconda channel if all package tests succeed.
+
+# install required packages
+conda install conda-build -y
+conda update conda -y
+conda update conda-build -y
+conda install anaconda-client -y
+
+# login to anaconda if anaconda username is provided
+if [ "$1" ]; then
+    conda config --set anaconda_upload yes
+    anaconda login --username "$1"
+else
+    conda config --set anaconda_upload no
+fi
+
+# install libgl1-mesa-glx in Linux. refre to 
+# https://github.com/conda-forge/pygridgen-feedstock/issues/10 for more information
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+   sudo apt update
+   sudo apt install libgl1-mesa-glx
+fi
+
+# add conda-forge channel to fetch/install required packages
+conda config --add channels conda-forge
+
+# build conda package
+conda build .
+
+# conda install --use-local occwl
