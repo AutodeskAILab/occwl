7c020d39e2f566f01627c88930922a683a5fe534
Pradeep Kumar Jayaraman <pradeep.kumar.jayaraman@autodesk.com>
2021-07-09 12:06:17 -0400

    Fixes to graph computation

--- src/occwl/graph.py
+++ src/occwl/graph.py
@@ -1,7 +1,4 @@
 from occwl.solid import Solid
-from occwl.face import Face
-from occwl.edge import Edge
-#from occwl.vertex import Vertex
 import itertools
 import networkx as nx
 from occwl.entity_mapper import EntityMapper
@@ -16,26 +13,32 @@ def face_adjacency(solid, self_loops=False):
         self_loops (bool, optional): Whether to add self loops in the graph. Defaults to False.
         
     Returns:
-        List[occwl.face.Face]: list of faces
-        Dict[(int, int), occwl.edge.Edge]: mapping from face pairs to edge that lies in-between them
-        List[(int, int)]: Indices into the face list; can be used as keys in the edge dict.
+        nx.Graph: Each B-rep face is mapped to a node with its index and each B-rep edge is mapped to an edge in the graph
+                  Node attributes:
+                  - "face": contains the B-rep face
+                  Edge attributes:
+                  - "edge": contains the B-rep edge
+                  - "edge_idx": index of the edge in the solid
     """
     assert isinstance(solid, Solid)
     mapper = EntityMapper(solid)
     graph = nx.Graph()
     for face in solid.faces():
         face_idx = mapper.face_index(face)
-        graph.add_node(face, index=face_idx)
+        graph.add_node(face_idx, face=face)
 
     for edge in solid.edges():
         connected_faces = list(solid.faces_from_edge(edge))
         if len(connected_faces) < 2:
             if edge.seam(connected_faces[0]) and self_loops:
-                graph.add_edge(connected_faces[0], connected_faces[0])
+                face_idx = mapper.face_index(connected_faces[0])
+                graph.add_edge(face_idx, face_idx)
         else:
-            for (fi, fj) in itertools.permutations(connected_faces):
+            for (face_i, face_j) in itertools.permutations(connected_faces):
+                face_i_index = mapper.face_index(face_i)
+                face_j_index = mapper.face_index(face_j)
                 edge_idx = mapper.edge_index(edge)
-                graph.add_edge(fi, fj, edge=edge, index=edge_idx)
+                graph.add_edge(face_i_index, face_j_index, edge=edge, edge_index=edge_idx)
     return graph
 
 
@@ -48,16 +51,19 @@ def vertex_adjacency(solid, self_loops=False):
         self_loops (bool, optional): Whether to add self loops in the graph. Defaults to False.
     
     Returns:
-        List[occwl.vertex.Vertex]: list of vertices
-        Dict[(int, int), occwl.edge.Edge]: mapping from vertex pairs to edge that lies in-between them
-        List[(int, int)]: Indices into the vertex list; can be used as keys in the edge dict.
+        nx.Graph: Each B-rep vertex is mapped to a node with its index and each B-rep edge is mapped to an edge in the graph
+                  Node attributes:
+                  - "vertex": contains the B-rep vertex
+                  Edge attributes:
+                  - "edge": contains the B-rep edge
+                  - "edge_idx": index of the edge in the solid
     """
     assert isinstance(solid, Solid)
     mapper = EntityMapper(solid)
     graph = nx.Graph()
     for vert in solid.vertices():
         vert_idx = mapper.vertex_index(vert)
-        graph.add_node(vert, index=vert_idx)
+        graph.add_node(vert_idx, vertex=vert)
 
     for edge in solid.edges():
         connected_verts = list(solid.vertices_from_edge(edge))
@@ -65,9 +71,12 @@ def vertex_adjacency(solid, self_loops=False):
             continue
         if len(connected_verts) < 2:
             if edge.closed_edge() and self_loops:
-                graph.add_edge(connected_verts[0], connected_verts[0])
+                vert_idx = mapper.vertex_index(connected_verts[0])
+                graph.add_edge(vert_idx, vert_idx)
         else:
-            for (vi, vj) in itertools.permutations(connected_verts):
+            for (vert_i, vert_j) in itertools.permutations(connected_verts):
                 edge_idx = mapper.edge_index(edge)
-                graph.add_edge(vi, vj, edge=edge, index=edge_idx)
+                vert_i_index = mapper.vertex_index(vert_i)
+                vert_j_index = mapper.vertex_index(vert_j)
+                graph.add_edge(vert_i_index, vert_j_index, edge=edge, edge_index=edge_idx)
     return graph
