11e5060a34c68d041f45bc455e931d41ada103e0
Pradeep Kumar Jayaraman <pradeep.kumar.jayaraman@autodesk.com>
2021-07-09 12:07:18 -0400

    Fix path

--- examples/ex1_hello_world.py
+++ examples/ex1_hello_world.py
@@ -1,12 +1,12 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from occwl.solid import Solid
 from occwl.viewer import Viewer
 
 box = Solid.make_box(10, 10, 10)
-v = Viewer()
+v = Viewer(backend="wx")
 v.display(box)
 v.fit()
 v.show()
--- examples/ex2_load_and_display_step.py
+++ examples/ex2_load_and_display_step.py
@@ -1,13 +1,14 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import pathlib
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from occwl.io import load_step
 from occwl.viewer import Viewer
 
 
-solid = load_step("example.stp")[0] # Returns a list of bodies from the step file
-v = Viewer()
+solid = load_step(pathlib.Path(__file__).resolve().parent.joinpath("example.stp"))[0] # Returns a list of bodies from the step file
+v = Viewer(backend="wx")
 v.display(solid)
 v.fit()
 v.show()
