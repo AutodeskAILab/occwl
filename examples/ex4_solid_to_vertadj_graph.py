e69f781734cd135135e42b77c1297694866b233a
Pradeep Kumar Jayaraman <pradeep.kumar.jayaraman@autodesk.com>
2021-07-08 22:30:13 -0400

    Use networkx graph

--- examples/ex4_solid_to_vertadj_graph.py
+++ examples/ex4_solid_to_vertadj_graph.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import numpy as np
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from occwl.solid import Solid
 from occwl.viewer import Viewer
@@ -9,23 +9,20 @@ from occwl.graph import face_adjacency, vertex_adjacency
 import math
 
 box = Solid.make_box(10, 10, 10)
-nodes, edges, connectivity = vertex_adjacency(box)
+g = vertex_adjacency(box, self_loops=True)
 
+v = Viewer(backend="wx")
 # Get the points for each vertex
-points = []
-for i, vert in enumerate(nodes):
+points = {}
+for i, vert in enumerate(g.nodes):
     pt = vert.point()
-    points.append(pt)
-
-# Make a sphere for each vertex
-v = Viewer()
-for i, vert in enumerate(nodes):    
-    v.display(Solid.make_sphere(center=points[i], radius=0.25))
+    points[g.nodes[vert]["index"]] = pt
+    v.display(Solid.make_sphere(center=pt, radius=0.25))
 
 # Make a cylinder for each edge connecting a pair of vertices
-for conn in connectivity:
-    pt1 = np.asarray(points[conn[0]])
-    pt2 = np.asarray(points[conn[1]])
+for vi, vj in g.edges:
+    pt1 = np.asarray(points[g.nodes[vi]["index"]])
+    pt2 = np.asarray(points[g.nodes[vj]["index"]])
     up_dir = pt2 - pt1
     v.display(Solid.make_cylinder(radius=0.2, height=np.linalg.norm(up_dir), base_point=pt1, up_dir=up_dir))
 
