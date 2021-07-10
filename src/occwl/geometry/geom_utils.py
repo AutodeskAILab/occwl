d51d1631511db0af68671fe492fd2a82499ff456
Pradeep Kumar Jayaraman <pradeep.kumar.jayaraman@autodesk.com>
2021-07-10 15:25:56 -0400

    Explicitly cast to float as np types won't work

--- src/occwl/geometry/geom_utils.py
+++ src/occwl/geometry/geom_utils.py
@@ -17,12 +17,12 @@ def gp_to_numpy(gp):
 
 def numpy_to_gp(np_point):
     assert np_point.size == 3
-    return gp_Pnt(np_point[0], np_point[1], np_point[2])
+    return gp_Pnt(float(np_point[0]), float(np_point[1]), float(np_point[2]))
 
 
 def numpy_to_gp_vec(np_vec):
     assert np_vec.size == 3
-    return gp_Vec(np_vec[0], np_vec[1], np_vec[2])
+    return gp_Vec(float(np_vec[0]), float(np_vec[1]), float(np_vec[2]))
 
 
 def numpy_to_gp_dir(np_vec):
